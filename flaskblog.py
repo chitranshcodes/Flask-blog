dab3af4fe97f26be198852febf102318463653d6
Chitransh <chitranshgaur@outlook.com>
2025-01-19 02:07:19 +0530

    Linked Html's and CSS's to .py file{TEMPLATES USED}

--- flaskblog.py
+++ flaskblog.py
@@ -1,9 +1,14 @@
-from flask import Flask
+from flask import Flask,render_template
 app=Flask(__name__)
 
 
-@app.route('/')
-def index():
-    return "<h1>Hello Flask first push</h1>"
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
 if __name__=="__main__":
     app.run(debug=True)
