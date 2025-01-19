fc5e357aca314c18e0bdf6853425ebbdf4cafbb2
Chitransh <chitranshgaur@outlook.com>
2025-01-19 12:55:11 +0530

    Layouts concept used

--- flaskblog.py
+++ flaskblog.py
@@ -16,7 +16,7 @@ def home():
 
 @app.route('/about')
 def about():
-    return render_template("about.html")
+    return render_template("about.html", title="About")
 
 if __name__=="__main__":
     app.run(debug=True)
