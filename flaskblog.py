b25a9df34c5c9981b334903a7186fe8aab8d168b
Chitransh <chitranshgaur@outlook.com>
2025-01-19 12:36:48 +0530

    First Dummy Post added

--- flaskblog.py
+++ flaskblog.py
@@ -1,10 +1,18 @@
 from flask import Flask,render_template
 app=Flask(__name__)
+posts=[
+{
+    "title":"First Post",
+    "author":"Chitransh Gaur",
+    "Date_Posted":"19/01/25",
+    "content":"aise hi likh rha hu ainwaiyyn"
+}
 
+]
 
 @app.route('/home')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', posts=posts)
 
 @app.route('/about')
 def about():
