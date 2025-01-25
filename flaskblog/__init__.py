9df512c24898c1183f731b0badc77e108b2a65e9
Chitransh <chitranshgaur@outlook.com>
2025-01-25 14:57:40 +0530

    bcrypt added to get hashed passwords

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 app=Flask(__name__)
 
@@ -7,5 +8,5 @@ app.config["SECRET_KEY"]="d7d187b1cd34edb71bbe5038d34e90ff"
 app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///site.db'
 
 db=SQLAlchemy(app)
-
+bcrypt=Brypt(app)
 from flaskblog import routes
