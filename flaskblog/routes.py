c7f63e9487731314162a487c0492ae9b8e52cff9
Chitransh <chitranshgaur@outlook.com>
2025-01-26 23:37:32 +0530

    login manager tried but not successful

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -45,6 +45,7 @@ def register():
 def login():
     form=LoginForm()
     if form.validate_on_submit():
+        
         if form.email.data=='xyz@gmail.com' and form.password.data=='password':
             flash('You have been successfully logged in !','success')
             return redirect(url_for('home'))
