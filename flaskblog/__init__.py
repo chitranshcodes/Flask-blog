69b3897124f072ae76019019d74b20107232b429
Chitransh <chitranshgaur@outlook.com>
2025-01-27 22:55:00 +0530

    Trash removed

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,7 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
-# from flask_login import LoginManager
 
 app=Flask(__name__)
 
@@ -10,6 +9,5 @@ app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///site.db'
 
 db=SQLAlchemy(app)
 bcrypt=Bcrypt(app)
-# login-manager==LoginManager(app)
 
 from flaskblog import routes
