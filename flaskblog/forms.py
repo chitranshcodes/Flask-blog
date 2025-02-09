36b7947cf552d85a9c765b8f564dcdd6110401e3
Chitransh <chitranshgaur@outlook.com>
2025-02-09 18:21:30 +0530

    profile pic input added in profile route

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -3,6 +3,7 @@ from wtforms import BooleanField, StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flaskblog.models import User
 from flask_login import current_user
+from flask_wtf.file import FileField, FileAllowed
 
 class RegistrationForm(FlaskForm):
     username=StringField('Username', validators=[DataRequired(), Length(min=6, max=20)])
@@ -32,6 +33,7 @@ class UpdationForm(FlaskForm):
     username=StringField('Username', validators=[DataRequired(), Length(min=6, max=20)])
     email = StringField("Email", validators=[DataRequired(), Length(min=5), Email()])
     submit=SubmitField('Update Now!')
+    picture= FileField('Upload Profile Picture', validators=[FileAllowed(['jpg','png'])])
 
     def validate_username(self,username):
         if username.data!=current_user.username: 
